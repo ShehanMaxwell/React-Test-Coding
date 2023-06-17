acaf9346561efbd616f58cdd9aaac7360da83e21
Shehan Maxwell <shehanbbn@gmail.com>
2023-06-17 21:15:47 +0530

    modified user.context.jsx in .

--- user.context.jsx
+++ user.context.jsx
@@ -9,21 +9,32 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: `SET_CURRENT_USER`,
+};
+
 const useReducer = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case 'SET_CURRENT_USER':
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
       return {
+        ...state,
         currentUser: payload,
       };
 
     default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
   }
 };
 
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [] = useReducer(useReducer, INITIAL_STATE);
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
